2548be204ea7fc40f3da84bc60b78586aa809547
Weimin wl328 <wl328@njit.edu>
2019-10-23 00:57:17 -0400

    Task: finish the function of the calculator

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,5 +1,59 @@
+def addition(a, b):
+    c = a + b
+    return c
+
+
+def subtraction(a, b):
+    c = a - b
+    return c
+
+
+def multiplication(a, b):
+    c = a * b
+    return c
+
+
+def division(a, b):
+    c = a / b
+    return c
+
+
+def square(a):
+    c = a ** 2
+    return c
+
+
+def square_root(a):
+    c = a ** 0.5
+    return c
+
+
 class Calculator:
     result = 0
 
     def __init__(self):
         pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
+
+    def multiplication(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+
+    def division(self, a, b):
+        self.result = division(a, b)
+        return self.result
+
+    def square(self, a):
+        self.result = square(a)
+        return self.result
+
+    def square_root(self, a):
+        self.result = square_root(a)
+        return self.result
